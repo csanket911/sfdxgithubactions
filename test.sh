d9646b13d82be2412ddd451a6991a0d072babdd3
Sanket Chatterjee <sanket.chatterjee@nutanix.com>
2020-08-02 21:19:27 +0530

    Update merge message

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 # checkout your feature branch and rebase from master
 git checkout test
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 # rebase from origin/master and resolve any conflicts
 env -i git pull --rebase origin master
 # switch to the dev branch
@@ -10,6 +11,6 @@ git checkout devint
 # (which could contain merged branches from other team members)
 env -i git pull --rebase origin devint
 # merge your branch
-git merge test
+git merge test -m "Merging $branch"
 # push changes to origin/devint (which triggers a deploy)
 git push origin devint
--- test_uat.sh
+++ test_uat.sh
@@ -2,14 +2,15 @@
 
 # checkout your feature branch and rebase from master
 git checkout test
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 # rebase from origin/master and resolve any conflicts
 env -i git pull --rebase origin master
 # switch to the dev branch
 git checkout uat
-# rebase from origin/devint
+# rebase from origin/uat
 # (which could contain merged branches from other team members)
 env -i git pull --rebase origin uat
 # merge your branch
-git merge test
+git merge test -m "Merging $branch"
 # push changes to origin/devint (which triggers a deploy)
 git push origin uat
